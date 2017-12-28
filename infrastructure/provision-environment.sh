0c6e07bcd632edc933bff6f58aded9a2aea12fc4
NathanCashmore <stat1cv0id>
2017-12-28 17:57:19 +0000

    Move travis / docker config to application

--- infrastructure/provision-environment.sh
+++ infrastructure/provision-environment.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+BLACK_ON_GREY=$'\e[47;30;1m'
+NONE=$'\e[0m'
+
 if [ $# -lt 2 ]
   then
     echo "Prerequisites:"
@@ -20,13 +23,13 @@ fi
 APP_NAME=$1
 SLACK_WEBHOOK_URL=$2
 
-echo "Provision state store"
+echo "${BLACK_ON_GREY}Provision state store${NONE}"
 aws s3 mb s3://staticvoid-co-uk-state-store
 
 export NAME=$APP_NAME.staticvoid.co.uk
 export KOPS_STATE_STORE=s3://staticvoid-co-uk-state-store
 
-echo "Provision cluster"
+echo "${BLACK_ON_GREY}Provision cluster${NONE}"
 kops create cluster \
     --node-count 1 \
     --zones eu-west-2a \
@@ -43,7 +46,7 @@ kops create cluster \
 kops update cluster ${NAME} --yes
 
 isValidCluster=`kops validate cluster`
-echo "Waiting for cluster to be valid..."
+echo "${BLACK_ON_GREY}Waiting for cluster to be valid...${NONE}"
 while [[ $isValidCluster != *"ready"* ]]
 do
     echo "Please wait... still building."
@@ -52,34 +55,32 @@ do
 done
 echo ".... cluster is READY"
 
-echo "Set kube context to created service"
+echo "${BLACK_ON_GREY}Set kube context to created service${NONE}"
 kubectl config use-context ${NAME}
 
-echo "Provision DNS extension"
+echo "${BLACK_ON_GREY}Update cluster with AWS Route 53 additional policies for DNS extension${NONE}"
+kops get cluster -o yaml > cluster-config.yaml
+sed $'s/spec:/spec:\\\n  additionalPolicies:\\\n    node: |\\\n      [\\\n        {\\\n          "Effect": "Allow",\\\n          "Action": ["route53:*"],\\\n          "Resource": ["*"]\\\n        }\\\n      ]/g' cluster-config.yaml > cluster-config-with-route-53.yaml
+kops replace -f cluster-config-with-route-53.yaml
+kops update cluster ${NAME} --yes
+kops rolling-update cluster
+rm cluster-config*.yaml
+
+echo "${BLACK_ON_GREY}Provision DNS extension${NONE}"
 kubectl apply -f ./kube/external-dns.yaml
 
-echo "Provision Slack notifications"
+echo "${BLACK_ON_GREY}Provision Slack notifications${NONE}"
 sed -e 's|SLACK_WEBHOOK_URL|'"${SLACK_WEBHOOK_URL}"'|g' ./kube/kube-slack.yaml > ./kube/${APP_NAME}-kube-slack.yaml
 kubectl apply -f ./kube/${APP_NAME}-kube-slack.yaml
 
-echo "Provision dashboard"
+echo "${BLACK_ON_GREY}Provision dashboard${NONE}"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
 
-echo "Provision Travis-CI"
-export KUBE_CA_CERT=$(kubectl config view --flatten --output=json \
-       | jq --raw-output '.clusters[0] .cluster ["certificate-authority-data"]')
-export KUBE_ADMIN_CERT=$(kubectl config view --flatten --output=json \
-       | jq --raw-output '.users[0] .user ["client-certificate-data"]')
-export KUBE_ADMIN_KEY=$(kubectl config view --flatten --output=json \
-       | jq --raw-output '.users[0] .user ["client-key-data"]')
-
-travis env set KUBE_CA_CERT $KUBE_CA_CERT
-travis env set KUBE_ADMIN_CERT $KUBE_ADMIN_CERT
-travis env set KUBE_ADMIN_KEY $KUBE_ADMIN_KEY
-
-echo "Export environment"
+echo "${BLACK_ON_GREY}Export environment${NONE}"
 
 mkdir -p ./env/
+
+adminpwd=`kops get secrets kube -oplaintext`
 
 echo "export NAME=${NAME}" > ./env/${APP_NAME}-env.sh
 echo "export KOPS_STATE_STORE=${KOPS_STATE_STORE}" >> ./env/${APP_NAME}-env.sh
@@ -89,8 +90,6 @@ echo "kubectl config use-context ${NAME}" >> ./env/${APP_NAME}-env.sh
 echo "To view the provisioned environment source the environment and goto kube dashboard ->"
 echo ""
 echo "source ./env/${APP_NAME}-env.sh"
-echo "kubectl proxy"
-echo "open http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
+echo "open http://api.${NAME}:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
 echo ""
-echo "NOTE : login with the admin details obtained using"
-echo "kubectl config view"
+echo "NOTE : login with the admin password ${adminpwd}"
